b9be4fab04d98db40c08c5d2aeadfe576ddc2208
Gradi Puata <gradipuata@gmail.com>
2026-05-03 20:37:42 +0200

    create dir for each entity config

--- simpli.Application/Interfaces/IIdentityService.cs
+++
@@ -1,8 +0,0 @@
-using simpli.Domain.Entities;
-
-public interface IIdentityService
-{
-  Task LoginAsync(LogDto dto);
-  Task RegisterAsync(CreateCompanyDto dto);
-  Task LogoutAsync();
-}
--- simpli.Infrastructure/Data/AppDbContext.cs
+++ simpli.Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using simpli.Domain;
 
 
 public class AppDbContext : IdentityDbContext<IdentityUser>
@@ -9,6 +10,14 @@ public class AppDbContext : IdentityDbContext<IdentityUser>
    :
    base(options)
   { }
+
+  //tables
+  public DbSet<Notification> Notifications { get; set; }
+  public DbSet<Company> Companies { get; set; }
+  public DbSet<Log> Logs { get; set; }
+  public DbSet<Room> Rooms { get; set; }
+  public DbSet<Visitor> Visitors { get; set; }
+
   protected override void OnModelCreating(ModelBuilder builder)
   {
     base.OnModelCreating(builder);
