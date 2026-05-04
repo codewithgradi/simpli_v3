3b3540c65d4db786afac0718d993e9c5905026f9
Gradi Puata <gradipuata@gmail.com>
2026-05-04 22:03:52 +0200

    added get all notisf

--- simpli.Application/Interfaces/INotificationRepo.cs
+++ simpli.Application/Interfaces/INotificationRepo.cs
@@ -2,7 +2,7 @@ using simpli.Domain.Entities;
 
 public interface INotification
 {
-    Task<List<NotificationDto>> GetAllNotifications(int company);
-    Task  MarkAllRead(int companyId);
-    Task  ClearAllNotifications(int companyId); 
+    Task<List<NotificationMessageDto>> GetAllNotifications(int company);
+    Task MarkAllRead(int companyId);
+    Task ClearAllNotifications(int companyId);
 }
--- simpli.Infrastructure/Repository/NotificationRepo.cs
+++ simpli.Infrastructure/Repository/NotificationRepo.cs
@@ -1,17 +1,26 @@
+using Microsoft.EntityFrameworkCore;
 using simpli.Domain.Entities;
 
 public class NotificationRepo : INotification
 {
     private readonly AppDbContext _context;
     private readonly NotificationMappers _mapper;
+    public NotificationRepo(AppDbContext context, NotificationMappers mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
     public Task ClearAllNotifications(int companyId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<List<NotificationDto>> GetAllNotifications(int company)
+    public async Task<List<NotificationMessageDto>> GetAllNotifications(int companyID)
     {
-        throw new NotImplementedException();
+        return await _mapper.ProjectToDto(
+            _context.Notifications.AsNoTracking()
+            .Where(x => x.CompanyId == companyID))
+            .ToListAsync();
     }
 
     public Task MarkAllRead(int companyId)
