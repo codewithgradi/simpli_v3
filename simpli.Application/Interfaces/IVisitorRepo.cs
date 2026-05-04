4771b7fe3fe069bc747e217a04a430ec8b34e966
Gradi Puata <gradipuata@gmail.com>
2026-05-04 19:10:29 +0200

    added get visitor on interface

--- simpli.Application/Interfaces/IVisitorRepo.cs
+++ simpli.Application/Interfaces/IVisitorRepo.cs
@@ -4,4 +4,5 @@ public interface IVisitorRepo
 {
     Task<VisitorDto> CheckIn(CheckInDto dto, int companyId, int roomId);
     Task CheckOut(CheckOutDto dto);
+    Task<VisitorDto> GetVisitor(int id);
 }
