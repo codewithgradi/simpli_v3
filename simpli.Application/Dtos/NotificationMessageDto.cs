e6023026e7f7d83cb070d60fc9c4213572512cb7
Gradi Puata <gradipuata@gmail.com>
2026-05-04 22:16:50 +0200

    fixed errors

--- simpli.Application/Dtos/NotificationMessageDto.cs
+++ simpli.Application/Dtos/NotificationMessageDto.cs
@@ -1,5 +1,6 @@
 public class NotificationMessageDto
 {
   public int Name { get; set; }
+  public DateTime CreatedAt { get; set; }
   public string? Message { get; set; } = "Successfully completed check-in at the front desk.";
 }
