12affe3657afb7018302c5d86ea9dc6766d6f904
Gradi Puata <gradipuata@gmail.com>
2026-05-04 19:02:45 +0200

    removed othersettings from env file cs

--- simpli.Infrastructure/Env/Env.cs
+++ simpli.Infrastructure/Env/Env.cs
@@ -1,10 +1,3 @@
-public class JwtSettings
-{
-    public string? Issuer { get; set; }
-    public string? Audience { get; set; }
-    public string? SigningKey { get; set; }
-}
-
 public class ConnnectionStrings
 {
     public string? DbDB { get; set; }
