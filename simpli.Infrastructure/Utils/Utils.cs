43ee43c6fb46cd5dccc22efd463af41a650dc45d
Gradi Puata <gradipuata@gmail.com>
2026-05-08 19:49:24 +0200

    added generatepascode

--- simpli.Infrastructure/Utils/Utils.cs
+++ simpli.Infrastructure/Utils/Utils.cs
@@ -1,5 +1,11 @@
+using System.Security.Cryptography;
+
 public static class Utils
 {
-  public static string GeneratePasscode() { return ""; }
+  public static string GeneratePasscode()
+  {
+    int passcode = RandomNumberGenerator.GetInt32(10000, 100000);
+    return passcode.ToString();
+  }
   public static void SendEmail() { }
 }
