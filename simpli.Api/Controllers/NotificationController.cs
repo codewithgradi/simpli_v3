cc2de8120a48e546577120612a4ba9bd28def7f9
Gradi Puata <gradipuata@gmail.com>
2026-05-15 22:26:46 +0200

    added mark all read

--- simpli.Api/Controllers/NotificationController.cs
+++ simpli.Api/Controllers/NotificationController.cs
@@ -20,5 +20,14 @@ namespace simpli.Api
       if (notifcations == null) return null;
       return Ok(notifcations);
     }
+
+    [HttpPatch("mark-read")]
+    public async Task<IActionResult> MarkAllRead()
+    {
+      var companyId = Convert.ToInt32(User.FindFirst("CompanyID"));
+      if (companyId == null) return null;
+      await _notificationRepo.MarkAllRead(companyId);
+      return Ok("Notifications have all be read.");
+    }
   }
 }
