c708d1a0a69adb5cd6b628e3f0b96fe5f7bcc048
Gradi Puata <gradipuata@gmail.com>
2026-05-15 22:12:52 +0200

    added maper

--- simpli.Application/Dtos/CreateNotificationDto.cs
+++ simpli.Application/Dtos/CreateNotificationDto.cs
@@ -2,6 +2,7 @@ namespace simpli.Application.Dtos
 {
   public class CreateNotificationDto
   {
+    public int CompanyId { get; set; }
     public string? VisitorName { get; set; }
     public VisitorStatus Status { get; set; }
   }
--- simpli.Application/Mappings/NotificationMappers.cs
+++ simpli.Application/Mappings/NotificationMappers.cs
@@ -1,10 +1,12 @@
 using Riok.Mapperly.Abstractions;
+using simpli.Application.Dtos;
 using simpli.Domain;
 using simpli.Domain.Entities;
 
 [Mapper]
 public partial class NotificationMappers
 {
+  public partial Notification MapToDtoFromCreate(CreateNotificationDto dto);
   public partial NotificationDto MapToDto(Notification notification);
   public partial Notification MapToEntity(NotificationDto notification);
   public partial IQueryable<NotificationMessageDto> ProjectToDto(IQueryable<Notification> query);
