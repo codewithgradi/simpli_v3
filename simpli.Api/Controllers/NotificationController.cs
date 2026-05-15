0329b0a0312e2c2a04bcb08140efe95c8f4efa4a
Gradi Puata <gradipuata@gmail.com>
2026-05-15 22:48:18 +0200

    added on api

--- simpli.Api/Controllers/NotificationController.cs
+++ simpli.Api/Controllers/NotificationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using simpli.Application.Dtos;
 
 namespace simpli.Api
 {
@@ -7,9 +8,11 @@ namespace simpli.Api
   public class NotificationController : ControllerBase
   {
     private readonly INotification _notificationRepo;
-    public NotificationController(INotification notification)
+    private readonly NotificationMappers _mapper;
+    public NotificationController(INotification notification, NotificationMappers mapper)
     {
       _notificationRepo = notification;
+      _mapper = mapper;
     }
     [HttpGet]
     public async Task<IActionResult> GetAllNotification()
@@ -37,6 +40,30 @@ namespace simpli.Api
       if (companyID == null) return Unauthorized();
       await _notificationRepo.ClearAllNotifications(companyID);
       return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateNotification(CreateNotificationDto notificationDto)
+    {
+      var companyID = Convert.ToInt32(User.FindFirst("CompanyID"));
+      if (companyID == null) return Unauthorized();
+      notificationDto.CompanyId = companyID;
+      var notisf = await _notificationRepo.CreateNotification(notificationDto);
+      var entity = _mapper.MapToDtoFromCreate(notificationDto);
+      if (notisf == null) return BadRequest("Could not create Notification");
+      return CreatedAtRoute(
+        nameof(GetOne),
+        new { Id = entity.Id },
+         _mapper.MapToDto(entity));
+
+    }
+
+    [HttpGet("{id:int}", Name = "GetOne")]
+    public async Task<IActionResult> GetOne([FromRoute] int id)
+    {
+      var notifcation = await _notificationRepo.GetNotification(id);
+      if (notifcation == null) return BadRequest("Could not get notification");
+      return Ok(notifcation)
     }
   }
 }
