67248e84e395408adfe6f86fb9e84925b5f147b1
Gradi Puata <gradipuata@gmail.com>
2026-05-16 14:01:33 +0200

    added getprofile in app layer

--- simpli.Application/Services/CompanyService.cs
+++ simpli.Application/Services/CompanyService.cs
@@ -1,3 +1,4 @@
+using simpli.Application.Dtos;
 using simpli.Domain.Entities;
 
 namespace simpli.Application.Services;
@@ -5,9 +6,17 @@ namespace simpli.Application.Services;
 public class CompanyService
 {
   private readonly ICompanyRepo _companyRepo;
-  public CompanyService(ICompanyRepo companyRepo)
+  private readonly CompanyMappers _mapper;
+
+  public CompanyService(ICompanyRepo companyRepo, CompanyMappers mapper)
   {
     _companyRepo = companyRepo;
+    _mapper = mapper;
+  }
+  public async Task<CompanyDto> GetCompanyProfile(int companyId)
+  {
+    var company = await _companyRepo.GetCompanyProfile(companyId);
+    return _mapper.MapToDto(company);
   }
 
 }
