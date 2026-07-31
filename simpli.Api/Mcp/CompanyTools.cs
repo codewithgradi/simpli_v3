57dc90fca18ec25d845691feaf770567d4e6f373
Gradi <gradipuata@gmail.com>
2026-07-31 08:13:13 +0200

    added CompanyTools

---
+++ simpli.Api/Mcp/CompanyTools.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using ModelContextProtocol.Server;
+using simpli.Application.Services;
+
+namespace simpli.Api.Mcp;
+
+[McpServerToolType]
+public class CompanyTools
+{
+    private readonly IServiceProvider _provider;
+
+    public CompanyTools(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
+    [McpServerTool(Name="update_company_details"),Description("This will soft delete company profile by setting the isDeleted property to true")]
+    public async Task SoftDeleteCompanyProfile(
+        [Description("This is company id from database")]
+        int companyId
+    )
+    {
+        await using var scope =  _provider.CreateAsyncScope();
+        var companyService =  scope.ServiceProvider.GetRequiredService<CompanyService>();
+        await companyService.SoftDeleteCompanyProfile(companyId);
+    }
+}
